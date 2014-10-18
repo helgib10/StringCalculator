430b2e30cf43d57b111218235d35022b94fe70cc
Helgi Bjornsson <helgi@i-c43143a5.greenqloud.internal>
2014-10-18 21:26:57 +0000

    import Lambdaj

--- stringcalculator/src/main/java/is/ru/stringcalculator/Calculator.java
+++ stringcalculator/src/main/java/is/ru/stringcalculator/Calculator.java
@@ -1,5 +1,7 @@
 package is.ru.stringcalculator;
 
+import ch.lambdaj.fucnction.convert.Converter;
+import static ch.lambdaj.Labda.*;
 import java.util.List;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
@@ -11,38 +13,45 @@ public class Calculator {
 		if (text.equals(""))
 			return 0;
 		int sum = 0;
-		List<Integer> numbers = split(text);
+		String[] tokens = split(text);
+		List<Integer> numbers = convert(tokens, toInt());
 		List<Integer> negatives = filter(lessThan(0), numbers);
-		if(negatives.length > 0)
+		if(negatives.size() > 0)
 		{
 			throw new RuntimeException("Negatives not allowed: " + join(negatives));
 		}
-		for (int i = 0; i < numbers.length; i++)
+		for (int i = 0; i < tokens.length; i++)
 		{
-			sum = sum + toInt(numbers[i]);
+			sum = sum + toInt(tokens[i]);
 		}
 		return sum;
 	}
-
-	private static String[] negative (String text){
-		if(
 
 	private static String[] split (String text){
 		if(text.startsWith("//")){
 			Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
 			m.matches();
 			String customSplitter = m.group(1);
-			String numbers = m.group(2);
-			return numbers.split(customSplitter);
+			String tokens = m.group(2);
+			return tokens.split(customSplitter);
 		}
 		else {
-			String[] numbers = text.split(",|\n");
-			return numbers;
+			String[] tokens = text.split(",|\n");
+			return tokens;
 		}
 	}
 
-	private static int toInt(String number){
-		return Integer.parseInt(number);
+	private static Converter<String, Integer> toInt() {
+		return new Converter<String, Integer>() {
+
+			public Integer convert(String from) {
+				return toInt(from);
+			}
+		};
+	}
+
+	private static int toInt(String tokens)throws NumberFormatException {
+		return Integer.parseInt(tokens);
 	}
 }
 
