1a85e96b15f6caffd0753da5ae0ea20050e9f22a
Helgi Bjornsson <helgi@i-c43143a5.greenqloud.internal>
2014-10-18 19:41:30 +0000

    prep for next test

--- stringcalculator/src/main/java/is/ru/stringcalculator/Calculator.java
+++ stringcalculator/src/main/java/is/ru/stringcalculator/Calculator.java
@@ -6,14 +6,19 @@ public class Calculator {
 		if (text.equals(""))
 			return 0;
 		int sum = 0;
-		String[] numbers = text.split(",|\n");
+		String[] numbers = split(text);
 		for (int i = 0; i < numbers.length; i++)
 		{
 			sum = sum + toInt(numbers[i]);
 		}
 		return sum;
 	}
-	
+
+	private static String[] split (String text){
+		String[] numbers = text.split(",|\n");
+		return numbers;
+	}
+
 	private static int toInt(String number){
 		return Integer.parseInt(number);
 	}
